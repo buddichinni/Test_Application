98bfe2c5697bdd9bc2f98b32b7c8c1a2629330f1
buddichinni <buddichinni@users.noreply.github.com>
2017-11-17 16:12:36 +0530

    Added hello
    Nothing

--- Test_Application/Program.cs
+++ Test_Application/Program.cs
@@ -7,6 +7,7 @@ namespace Test_Application
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello");
         }
     }
 }
